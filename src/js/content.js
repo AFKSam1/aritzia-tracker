22c3d7f6031a210032bcba3d6341b46c38c6d74c
Samuel Arsenault <arsenault.samuel.5@courrier.uqam.ca>
2023-09-19 23:59:12 -0400

    refactoring

--- src/js/content.js
+++ src/js/content.js
@@ -1,146 +1,84 @@
 const parsers = {
     aritzia: function (doc) {
-        let parsedData = {};
-        const titleElement = doc.querySelector("meta[property='og:title']");
-        if (titleElement) {
-            parsedData.title = titleElement.getAttribute("content");
-        }
+        const parsedData = extractMetaData(doc, [
+            { property: "og:title", key: "title" },
+            { property: "og:price:amount", key: "price" },
+            { property: "og:description", key: "description" },
+            { property: "og:image", key: "imgUrl" }
+        ]);
 
-        // Extract price
-        const priceElement = doc.querySelector("meta[property='og:price:amount']");
-        if (priceElement) {
-            parsedData.price = priceElement.getAttribute("content");
-        }
-
-        // Extract description
-        const descriptionElement = doc.querySelector("meta[property='og:description']");
-        if (descriptionElement) {
-            parsedData.description = descriptionElement.getAttribute("content");
-        }
-        const imgUrl = doc.querySelector("meta[property='og:image']");
-        if (imgUrl) {
-            parsedData.imgUrl = imgUrl.getAttribute("content");
-        }
-
-        // Get all the 'li' elements with the class 'ar-dropdown__option' from the document
-        const listItems = doc.querySelectorAll(".ar-dropdown__option");
-
-        // Initialize an array to hold sizes and inventory statuses
-        const stockInfo = [];
-
-        // Loop through each 'li' element
-        listItems.forEach((item) => {
-            // Find the 'span' with class 'f1' inside each 'li'
-            const sizeSpan = item.querySelector(".f1");
-
-            // Find the 'span' with class 'js-size-dropdown__inventory-status' inside each 'li'
-            const inventorySpan = item.querySelector(".js-size-dropdown__inventory-status");
-
-            // If both 'span' elements exist, capture their text content
-            if (sizeSpan && sizeSpan.textContent && inventorySpan) {
-                const size = sizeSpan.textContent.trim();
-                var inventoryStatus = inventorySpan.textContent.trim();
-                if (inventoryStatus == "") {
-                    inventoryStatus = "In Stock";
-                }
-
-                // Store the size and its inventory status in an object, and add it to the array
-                stockInfo.push({ size, inventoryStatus });
-            }
-        });
-
-        // Log the sizes array to the console
-        console.log(stockInfo);
-        parsedData.stockInfo = stockInfo;
-
-        console.log(parsedData);
+        parsedData.stockInfo = extractStockInfoAritzia(doc);
         return parsedData;
     },
     primitiveskate: function (doc) {
-        // Fetch and parse the document using jsdom or other methods to get 'doc' as a DOM object.
-        // Assuming 'doc' is your parsed DOM object...
-
-        let parsedData = {};
-
-        // Extract JSON object embedded in the script tag
+        const parsedData = {};
         const scriptElement = doc.querySelector('script[data-section-type="static-product"]');
         if (scriptElement) {
-            const jsonText = scriptElement.innerHTML;
-            const jsonObj = JSON.parse(jsonText);
-
-            // Extract relevant product information
+            const jsonObj = JSON.parse(scriptElement.innerHTML);
             parsedData.title = jsonObj.product.title;
-            //Froms cents to dollars
-            parsedData.price = (jsonObj.product.price/100).toFixed(2);
-            parsedData.imgUrl ="https:"+(jsonObj.product.featured_image);
-            // parsedData.imgUrl = "https://primitiveskate.com/cdn/shop/products/PAPHO2123-SKINLSTEE-BLK-F.jpg?v=1646946822"
-            // console.log(jsonObj.product.featured_image).slice(2)
-            // console.log(parsedData.imgUrl)
-            // Extract stock information
-            const stockInfo = jsonObj.product.variants.map((variant) => {
-                return {
-                    size: variant.title,
-                    inventoryStatus: variant.inventory_quantity,
-                };
-            });
-            parsedData.stockInfo = stockInfo;
+            parsedData.price = (jsonObj.product.price / 100).toFixed(2);
+            parsedData.imgUrl = `https:${jsonObj.product.featured_image}`;
+            parsedData.stockInfo = jsonObj.product.variants.map(({ title, inventory_quantity }) => ({
+                size: title,
+                inventoryStatus: inventory_quantity,
+            }));
         }
-
-        console.log(parsedData);
         return parsedData;
     },
-    simons: function(doc){
-        let parsedData = {};
-
-        // Find the element with class "selected" and extract its data-product-color-id attribute
-        const selectedColorElement = doc.querySelector('.single-product-color.js-color.selected');
-        if (selectedColorElement) {
-            parsedData.productColorId = selectedColorElement.getAttribute('data-product-color-id');
-        }
-
-        // Find and extract the script content
-        const scriptContent = Array.from(doc.querySelectorAll('script'))
-            .map(el => el.textContent)
-            .join('\n');
-        
-        const regex = /product\.maxStocks\s*=\s*(\{[\s\S]*?\});/;  // Regular expression to match product.maxStocks
-        const match = scriptContent.match(regex);
-        const regex2 = /product\.sizesEn\s*=\s*(\{[\s\S]*?\});/;  // Regular expression to match product.sizesEn
-        const match2 = scriptContent.match(regex2);
-        var sizesEn;
-
-        if(match2 && match2[1]){
-            sizesEn = JSON.parse(match2[1]);
-            console.log(sizesEn)
-        }
-
-        if (match && match[1]) {
-            const maxStocks = JSON.parse(match[1]);
-            console.log("max stocks:",maxStocks);
-            var mapped = mapColorsToStocks(doc,maxStocks,sizesEn);
-        console.log("mapped :",mapped);
-        }
-        parsedData.stockInfo = mapped;
-
-        const imgUrl = doc.querySelector("meta[itemprop='image']");
-        if (imgUrl) {
-            parsedData.imgUrl = imgUrl.getAttribute("content");
-        }
-        const titleElement = doc.querySelector("meta[itemprop='name']");
-        if (titleElement) {
-            parsedData.title = titleElement.getAttribute("content");
-        }
-        const price = doc.querySelector(".single-product-price");
-        if(price){
-            parsedData.price = parseFloat(price.innerText)
-        }
-
+    simons: function (doc) {
+        const parsedData = extractMetaData(doc, [
+            { itemprop: "image", key: "imgUrl" },
+            { itemprop: "name", key: "title" }
+        ], "itemprop");
     
-        console.log(parsedData);
+        // Extracting the price
+        const priceElement = doc.querySelector(".single-product-price");
+        if (priceElement) {
+            parsedData.price = parseFloat(priceElement.innerText);
+        }
+    
+        const scriptContent = Array.from(doc.querySelectorAll('script')).map(el => el.textContent).join('\n');
+        const maxStocks = extractJsonFromScript(scriptContent, /product\.maxStocks\s*=\s*(\{[\s\S]*?\});/);
+        const sizesEn = extractJsonFromScript(scriptContent, /product\.sizesEn\s*=\s*(\{[\s\S]*?\});/);
+    
+        parsedData.stockInfo = mapColorsToStocks(doc, maxStocks, sizesEn);
         return parsedData;
     },
+    
     // ...
 };
+
+// ... (rest of the code remains the same)
+
+
+function extractMetaData(doc, metaList, attribute = "property") {
+    return metaList.reduce((acc, { property, key, itemprop }) => {
+        const element = doc.querySelector(`meta[${attribute}='${property || itemprop}']`);
+        if (element) {
+            acc[key] = element.getAttribute("content");
+        }
+        return acc;
+    }, {});
+}
+
+function extractJsonFromScript(scriptContent, regex) {
+    const match = scriptContent.match(regex);
+    return match ? JSON.parse(match[1]) : null;
+}
+
+function extractStockInfoAritzia(doc) {
+    return Array.from(doc.querySelectorAll(".ar-dropdown__option")).map(item => {
+        const sizeSpan = item.querySelector(".f1");
+        const inventorySpan = item.querySelector(".js-size-dropdown__inventory-status");
+        if (sizeSpan && inventorySpan) {
+            const size = sizeSpan.textContent.trim();
+            let inventoryStatus = inventorySpan.textContent.trim() || "In Stock";
+            return { size, inventoryStatus };
+        }
+        return null;
+    }).filter(Boolean);
+}
+
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     if (request.action === "parseHtml") {
@@ -155,7 +93,7 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 
 function parseDomainHtml(domain, doc) {
     const parser = parsers[domain];
-    return parser(doc);
+    return parser ? parser(doc) : null;
 }
 
 function mapColorsToStocks(doc, maxStocks,sizesEn) {
